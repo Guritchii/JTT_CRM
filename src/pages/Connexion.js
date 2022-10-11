012a866404905149b642cad508620f7ff8e9c57f
Maxence LANONE <maxence.lanone@etu.uca.fr>
2022-10-11 07:14:43 +0000

    Transférer les fichiers vers 'src/pages'

---
+++ src/pages/Connexion.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Connexion = () => {
+    return (
+        <div>
+            {/* Create a connexion page */}
+            <img  srcset="./LogoApp.svg"></img>
+            <div className="connexion">
+                <form action="">
+                    <input type="text" placeholder="Pseudo" />
+                    <input type="password" placeholder="Mot de passe" />
+                    <button type="submit">Se connecter</button>
+                    <input type="checkbox"/>
+                    <label htmlFor="checkbox">Se souvenir de moi</label>
+                </form>
+            </div>
+            <a href="http://localhost">Mot de passe oublié ?</a>
+        </div>
+    );
+};
+
+export default Connexion;
