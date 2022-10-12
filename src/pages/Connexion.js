8e3e8d7293bf6ea228aa7a05ab59ca7a2e3c0d10
Maxence Lanone <maxence.lanone@etu.uca.fr>
2022-10-12 14:10:12 +0200

    finition page de connexion (partie front ok)

--- src/pages/Connexion.js
+++ src/pages/Connexion.js
@@ -3,19 +3,30 @@ import React from 'react';
 
 const Connexion = () => {
     return (
-        <div>
+        <div className="page_connexion">
             {/* Create a connexion page */}
-            <img  srcset="./LogoApp.svg"></img>
-            <div className="connexion">
-                <form action="">
-                    <input type="text" placeholder="Pseudo" />
-                    <input type="password" placeholder="Mot de passe" />
-                    <button type="submit">Se connecter</button>
-                    <input type="checkbox"/>
-                    <label htmlFor="checkbox">Se souvenir de moi</label>
-                </form>
-            </div>
-            <a href="http://localhost">Mot de passe oublié ?</a>
+            <img className="logo" srcSet="./LogoApp.svg"></img>
+            <form action="">
+                <table className="formulaire_de_connexion">
+                    <tr className="connexion_text">
+                        Connexion
+                    </tr>
+                    <tr>
+                        <input className="text_zone" type="text" placeholder="Pseudo" />
+                    </tr>
+                    <tr>
+                        <input className="text_zone" type="password" placeholder="Mot de passe" />
+                    </tr>
+                    <tr className="envoyer">
+                        <div className="memory_me">
+                            <label htmlFor="checkbox">Se souvenir de moi</label>
+                            <input type="checkbox" />
+                        </div>
+                        <button type="submit">Se connecter</button>
+                    </tr>
+                </table>
+            </form>
+            <a className="forgot_pw" href="http://localhost">Mot de passe oublié ?</a>
         </div>
     );
 };
