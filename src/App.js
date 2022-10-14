55938ca0068fa7825edd5b90f70c66124e63951c
clverdoire <Clement.verdoire@etu.uca.fr>
2022-10-14 08:50:00 +0200

    Ajout début page Admin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Connexion from './pages/Connexion';
+/*import Admin from './pages/Admin.js';*/
 
 const App = () => {
   return (
---
+++ src/pages/Admin.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Admin = () => {
+    return (
+        <div className="page_admin">
+            {/* Create a admin page */}
+            <div className="nav_bar_verticale"></div>
+                <img className="logo" srcSet="./LogoApp.svg"></img>
+            <div className="create_user"></div>
+        </div>
+    );
+};
+
+export default Admin;
