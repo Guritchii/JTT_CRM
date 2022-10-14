a648c1a4375ae131399a524b1a58ea85f2017b23
clverdoire <Clement.verdoire@etu.uca.fr>
2022-10-14 09:25:39 +0200

    Page Admin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Connexion from './pages/Connexion';
-/*import Admin from './pages/Admin.js';*/
+// import Connexion from './pages/Connexion';
+import Admin from './pages/Admin.js';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<Connexion/>} />
+        <Route path="*" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -5,8 +5,9 @@ const Admin = () => {
     return (
         <div className="page_admin">
             {/* Create a admin page */}
-            <div className="nav_bar_verticale"></div>
-                <img className="logo" srcSet="./LogoApp.svg"></img>
+            <div className="nav_bar_verticale">
+                nav_bar
+            </div>
             <div className="create_user"></div>
         </div>
     );
