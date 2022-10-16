c07950d124b30f98059ecee24aea8ea0b5b2fe55
jeremy.ducourthial <jeremy.ducourthial@etu.uca.fr>
2022-10-16 15:37:08 +0200

    Ajout du serveur api en node js et  création de la base MYSQL sur freemysqlhosting.net

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Connexion from './pages/Connexion';
+import Connexion from './pages/Connexion';
 import Admin from './pages/Admin.js';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    //<BrowserRouter>
+    //  <Routes>
+    //    <Route path="*" element={<Connexion />} />
+    //  </Routes>
+    //</BrowserRouter>
+
+    <Connexion />
   );
 }
 
