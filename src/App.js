23fb28a76ed4d1e6b2be1791d296c2d503b61d6b
Jeremy DUCOURTHIAL <Jeremy.DUCOURTHIAL@etu.uca.fr>
2022-10-17 19:48:29 +0200

    Connexion de la base de donnée avec la page connexion. Vérification avec mot de passe et login.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Connexion from './pages/Connexion';
-// import Admin from './pages/Admin.js';
+import Admin from './pages/Admin.js';
 
 const App = () => {
   return (
-    //<BrowserRouter>
-    //  <Routes>
-    //    <Route path="*" element={<Connexion />} />
-    //  </Routes>
-    //</BrowserRouter>
-
-    <Connexion />
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Connexion />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
