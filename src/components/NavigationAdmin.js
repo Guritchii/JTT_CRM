2092568a4b0db615f45ae63966b6a3d1dffd6868
Theo DUPIN <Theo.DUPIN@etu.uca.fr>
2022-10-19 17:05:15 +0200

    Finalisation Page Admin

--- src/components/NavigationAdmin.js
+++ src/components/NavigationAdmin.js
@@ -7,13 +7,13 @@ const NavigationAdmin = () => {
         <div className="nav_bar_verticale">
             <img className="logo" srcSet="./LogoApp.svg"></img>
             <NavLink className="create_user" to="/Admin">
-                <button id="user" className="bouton" type="button" value="Créer un utilisateur" onClick="Create_user()">Créer un nouvel utilisateur</button>
+                <button id="user" className="bouton" type="button" value="Créer un utilisateur" onClick="Create_user()">Créer utilisateur</button>
             </NavLink>
             <NavLink className="modify_user" to="/Admin_modif">
-                <button id="user" className="bouton" type="button" value="Modifier un utilisateur" /*onClick="Modify_user()"*/>Modifier un utilisateur</button>
+                <button id="user" className="bouton" type="button" value="Modifier un utilisateur" /*onClick="Modify_user()"*/>Modifier utilisateur</button>
             </NavLink>
             <NavLink className="delete_user" to="/Admin_supp">
-                <button id="user" className="bouton" type="button" value="Supprimer un utilisateur" /*onClick="Delete_user()"*/>Supprimer un utilisateur</button>
+                <button id="user" className="bouton" type="button" value="Supprimer un utilisateur" /*onClick="Delete_user()"*/>Supprimer utilisateur</button>
             </NavLink>
         </div>
 
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -7,40 +7,41 @@ const Admin = () => {
         <div className="page_admin">
             <NavigationAdmin />
             {/* Create a admin page */}
-            <p className="Titre">Admin
-            <div className="Formulaire">
-                <form className="form">
-                    <table className="Formulaire_de_connexion">
-                        <tr>
-                            <p className="texte">Nom :</p>
-                            <input id="nom" className="texte_zone" type="text" placeholder="Nom"/>
-                        </tr>
-                        <tr>
-                            <p className="texte">Prénom :</p>
-                            <input id="prénom" className="texte_zone" type="text" placeholder="Prénom"/>
-                        </tr>
-                        <tr>
-                            <p className="texte">Rôle dans l'entreprise :</p>
-                            <input id="rôle" className="texte_zone" type="text" placeholder="Rôle"/>
-                        </tr>
-                        <tr>
-                            <p className="texte">Identifiant :</p>
-                            <input id="identifiant" className="texte_zone" type="text" placeholder="Identifiant"/>
-                        </tr>
-                        <tr>
-                            <p className="texte">Mot de passe (par défaut) :</p>
-                            <input id="password" className="texte_zone" type="password" placeholder="Mot de passe (par default)"/>
-                        </tr>
-                        <tr>
-                            <div className='bouton_submit'>
-                                <button className="bouton_val_ann" type="submit">Valider</button>
-                                <button className="bouton_val_ann" type="submit">Annuler</button>
-                            </div>
-                        </tr>
-                    </table>
-                </form>
+            <div className="Titre_Formulaire">
+                <p className="Titre">Admin</p>
+                <div className="Formulaire">
+                    <form className="form">
+                        <table className="Formulaire_de_connexion">
+                            <tr>
+                                <p className="texte">Nom :</p>
+                                <input id="nom" className="texte_zone" type="text" placeholder="Nom"/>
+                            </tr>
+                            <tr>
+                                <p className="texte">Prénom :</p>
+                                <input id="prénom" className="texte_zone" type="text" placeholder="Prénom"/>
+                            </tr>
+                            <tr>
+                                <p className="texte">Rôle dans l'entreprise :</p>
+                                <input id="rôle" className="texte_zone" type="text" placeholder="Rôle"/>
+                            </tr>
+                            <tr>
+                                <p className="texte">Identifiant :</p>
+                                <input id="identifiant" className="texte_zone" type="text" placeholder="Identifiant"/>
+                            </tr>
+                            <tr>
+                                <p className="texte">Mot de passe (par défaut) :</p>
+                                <input id="password" className="texte_zone" type="password" placeholder="Mot de passe (par default)"/>
+                            </tr>
+                            <tr>
+                                <div className='bouton_submit'>
+                                    <button className="bouton_val" type="submit">Valider</button>
+                                    <button className="bouton_ann" type="submit">Annuler</button>
+                                </div>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
             </div>
-            </p>
         </div>
     );
 };
