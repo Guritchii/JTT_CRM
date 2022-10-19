d460a3a386205ac2d8388e928fb3490731cc666d
Theo DUPIN <Theo.DUPIN@etu.uca.fr>
2022-10-19 21:04:28 +0200

    Changement Page Admin_Supp

--- src/pages/Admin_supp.js
+++ src/pages/Admin_supp.js
@@ -4,29 +4,43 @@ import NavigationAdmin from '../components/NavigationAdmin.js';
 const Admin_supp = () => {
     return (
         <div className="page_admin">
+            <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"></link>
             {/* Create a page to delete an user in the admin page*/}
-            <NavigationAdmin/>
-            <p className="Titre">Admin</p>
-            <form className="formulaire">
-                <table className="Formulaire_de_recherche">
-                    <tr>
-                        <input id="personne" type="search" className="texte_barre" placeholder="🔎 Rechercher un utilisateur" /*onClick={tab}*//>
-                    </tr>
-                    <tr>
-
-                    </tr>
-                </table>
-            </form>
-        </div>
+            <NavigationAdmin />
+            <div className="Titre_Formulaire_Rech">
+                <p className="Titre">Admin</p>
+                <div className="rechLogo">
+                    <div className="input_box">
+                    <input type="search" placeholder="Rechercher..."/>
+                        <span className="search">
+                            <i class="uil uil-search search-icon"></i>
+                        </span>
+                    </div>
+                    <form className="formulaire">
+                        <table className="Formulaire_de_recherche">
+                            <tr className="tr_bouton">
+                                <div className="bouton_personnes">
+                                    <p className="bouton_personne1">
+                                        <strong>Dupont Jacques</strong>
+                                    </p>
+                                    <p className="bouton_personne2">
+                                        <strong>Carreau Alexis</strong>
+                                    </p>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div id="style" className='bouton_submit'>
+                                    <button className="bouton_sup" type="submit" onClick="document.getElementById('style').style.backgroundColor='green'">Supprimer</button>
+                                    <button className="bouton_ann" type="submit" onClick="document.getElementById('style').style.backgroundColor='red'">Annuler</button>
+                                </div>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
+            </div>
+         </div>
     );
 };
 
-/*function tab(){
-    var tab = new Array();
-    if(Create_user()){
-        tab.push(nom, prenom);
-    }
-    console.log(tab);
-}*/
 
 export default Admin_supp;
