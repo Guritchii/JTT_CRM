6433913af273db3688e2ea64652642c99918103d
Theo DUPIN <Theo.DUPIN@etu.uca.fr>
2022-10-20 16:56:07 +0200

    Finalisation Page Admin

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -13,34 +13,28 @@ const Admin = () => {
                     <form className="form">
                         <table className="Formulaire_de_connexion">
                             <tr>
-                                <p className="texte">Nom :</p>
-                                <input id="nom" className="texte_zone" type="text" placeholder="Nom..."/>
-                            </tr>
-                            <tr>
-                                <p className="texte">Prénom :</p>
-                                <input id="prénom" className="texte_zone" type="text" placeholder="Prénom..."/>
-                            </tr>
-                            <tr>
-                                <p className="texte">Rôle dans l'entreprise :</p>
-                                <input id="rôle" className="texte_zone" type="text" placeholder="Rôle..."/>
-                            </tr>
-                            <tr>
-                                <p className="texte">Identifiant :</p>
-                                <input id="identifiant" className="texte_zone" type="text" placeholder="Identifiant..."/>
-                            </tr>
-                            <tr>
-                                <p className="texte">Mot de passe (par défaut) :</p>
-                                <input id="password" className="texte_zone" type="password" placeholder="Mot de passe (par default)..."/>
-                            </tr>
-                            <tr>
-                                <div className='bouton_submit'>
-                                    <button className="bouton_val" type="submit">Valider</button>
-                                    <button className="bouton_ann" type="submit">Annuler</button>
+                                <div className="texte_côté">
+                                    <p>Nom :</p>
+                                    <p>Prénom :</p>
+                                    <p>Rôle :</p>
+                                    <p>Identifiant :</p>
+                                    <p>Mot de passe :</p>
                                 </div>
+                            </tr>
+                            <tr>
+                                <input id="nom" className="texte_zone" type="text" placeholder="Nom..."/>
+                                <input id="prénom" className="texte_zone" type="text" placeholder="Prénom..."/>
+                                <input id="rôle" className="texte_zone" type="text" placeholder="Rôle..."/>
+                                <input id="identifiant" className="texte_zone" type="text" placeholder="Identifiant..."/>
+                                <input id="password" className="texte_zone" type="password" placeholder="Mot de passe (par default)..."/>
                             </tr>
                         </table>
                     </form>
                 </div>
+                <tr className="bouton_submit">
+                    <button className="bouton_val" type="submit">Valider</button>
+                    <button className="bouton_ann" type="submit">Annuler</button>
+                </tr>
             </div>
         </div>
     );
