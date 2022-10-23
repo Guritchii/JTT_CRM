e49c5facb12c0625ed9f4319d02b6a4e09b5281a
jeremy.ducourthial <jeremy.ducourthial@etu.uca.fr>
2022-10-23 17:26:51 +0200

    Ajout api post + utilisation sur la page admin

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import axios from 'axios'
 import NavigationAdmin from '../components/NavigationAdmin.js';
 
+const api = axios.create({
+    baseURL: 'http://localhost:8080'
+  })
 
-const Admin = () => {
+function Admin() {
+
+    function checkAdd(event){
+        
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        const values = Object.fromEntries(formData.entries());
+
+        console.log(values.firstname);
+
+        
+
+        api.post('/User/Add', values).then (function(response) {
+        console.log(response.data);
+        });
+    };
+
     return (
         <div className="page_admin">
             <NavigationAdmin />
@@ -10,7 +31,7 @@ const Admin = () => {
             <div className="Titre_Formulaire">
                 <p className="Titre">Admin</p>
                 <div className="Formulaire">
-                    <form className="form">
+                    <form className="form" onSubmit={checkAdd}>
                         <table className="Formulaire_de_connexion">
                             <tr>
                                 <div className="texte_côté">
@@ -22,19 +43,19 @@ const Admin = () => {
                                 </div>
                             </tr>
                             <tr>
-                                <input id="nom" className="texte_zone" type="text" placeholder="Nom..."/>
-                                <input id="prénom" className="texte_zone" type="text" placeholder="Prénom..."/>
-                                <input id="rôle" className="texte_zone" type="text" placeholder="Rôle..."/>
-                                <input id="identifiant" className="texte_zone" type="text" placeholder="Identifiant..."/>
-                                <input id="password" className="texte_zone" type="password" placeholder="Mot de passe (par default)..."/>
+                                <input id="nom" name='lastname' className="texte_zone" type="text" placeholder="Nom..."/>
+                                <input id="prénom" name='firstname' className="texte_zone" type="text" placeholder="Prénom..."/>
+                                <input id="rôle" name='idrole' className="texte_zone" type="text" placeholder="Rôle..."/>
+                                <input id="identifiant" name='login' className="texte_zone" type="text" placeholder="Identifiant..."/>
+                                <input id="password" name='password' className="texte_zone" type="password" placeholder="Mot de passe (par default)..."/>
                             </tr>
                         </table>
+                        <div className="bouton_submit">
+                            <button className="bouton_val" type="submit">Valider</button>
+                            <button className="bouton_ann" type='reset'>Annuler</button>
+                        </div>
                     </form>
                 </div>
-                <tr className="bouton_submit">
-                    <button className="bouton_val" type="submit">Valider</button>
-                    <button className="bouton_ann" type="submit">Annuler</button>
-                </tr>
             </div>
         </div>
     );
