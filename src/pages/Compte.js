f69162fb3d7d5d88a417dfda4446f0a84f9f5037
Theo DUPIN <Theo.DUPIN@etu.uca.fr>
2022-11-15 15:47:25 +0100

    page compte

--- src/pages/Compte.js
+++ src/pages/Compte.js
@@ -1,3 +1,5 @@
+import { Component } from '@fullcalendar/core';
+import { render } from '@testing-library/react';
 import { click } from '@testing-library/user-event/dist/click';
 import React from 'react';
 import NavigationDashboard from '../components/NavigationDashboard';
@@ -9,29 +11,29 @@ const Compte = () => {
         <body>
 
             <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"></link>
-            
+
             <div className="page_compte">
                 {/* Create an account page */}
                 <div className="haut_de_page">
                     <h2 className="titre">Mon Compte</h2>
                     <div className="rechLogo">
                         <div className="input_box">
-                            <input type="search" placeholder="Rechercher..."/>
+                            <input type="search" placeholder="Rechercher..." />
                             <span className="search">
                                 <i class="uil uil-search search-icon"></i>
                             </span>
                         </div>
-                        <img className="logo" srcSet="./LogoApp.svg"/>
+                        <img className="logo" srcSet="./LogoApp.svg" />
                     </div>
                 </div>
                 <div className="bas_de_page">
-                    <NavigationDashboard />                      
+                    <NavigationDashboard />
                     <div className="Compte">
                         <div className="name_picture">
                             <div className="picture">
                                 <div id="display_image"></div>
                                 <div className='bouton_submit'>
-                                    <input type="file" name="fileUpload" id="fileUpload"/>
+                                    <input type="file" name="fileUpload" id="fileUpload" accept="image/*"/>
                                 </div>
                             </div>
                             <div className="name">
@@ -46,30 +48,30 @@ const Compte = () => {
                         </div>
                         <div className="infoPerso">
                             <p className="description">Informations personnel</p>
-                            <div className="parti_mail">   
+                            <div className="parti_mail">
                                 <p className="def">Mail :</p>
                                 <p className="mail">coucou@gmail.com</p>
                             </div>
-                            <div className="parti_pays">                                
+                            <div className="parti_pays">
                                 <p className="def">Pays ou Région :</p>
                                 <p className="pays_region">France</p>
                             </div>
-                            <div className="parti_tel">                       
+                            <div className="parti_tel">
                                 <p className="def">Tel :</p>
                                 <p className="tel">0745632114</p>
                             </div>
                         </div>
                         <div className="infoEntreprise">
                             <p className="description">Informations entreprise</p>
-                            <div className="parti_name">   
+                            <div className="parti_name">
                                 <p className="def">Nom de l'entreprise</p>
                                 <p className="name">entreprise name</p>
                             </div>
-                            <div className="parti_secteurAct">                                
+                            <div className="parti_secteurAct">
                                 <p className="def">Secteur d'activité :</p>
                                 <p className="secteurAct">Coiffure</p>
                             </div>
-                            <div className="parti_nbClient">                       
+                            <div className="parti_nbClient">
                                 <p className="def">Nombre clients :</p>
                                 <p className="nbClient">200</p>
                             </div>
@@ -79,30 +81,8 @@ const Compte = () => {
             </div>
         </body>
     );
+
 };
 
-let fileUpload = document.getElementById("fileUpload");
-let display_image = document.getElementById("display_image");
-
-//preview();
-
-function preview(){
-    display_image.innerHTML = "";
-
-    for(fileUpload of fileUpload.files){
-        let reader = new FileReader();
-        let figure = document.createElement("figure");
-        let figCap = document.createElement("figcaption");
-        figCap.innerText = fileUpload.name;
-        figure.appendChild(figCap);
-        reader.onload=()=>{
-            let img = document.createElement("img");
-            img.setAttribute("src", reader.result);
-            figure.insertBefore(img,figCap);
-        }
-        display_image.appendChild(figure);
-        reader.readAsDataURL(fileUpload);
-    }
-}
 
 export default Compte;
