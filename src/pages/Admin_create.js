2130aa56b11e839c92e3dd5482eae5cb99cf95a7
clément <clement.verdoire@etu.uca.fr>
2022-12-09 12:11:48 +0100

    gestion bug affichage ensemble page admin

--- src/pages/Admin_create.js
+++ src/pages/Admin_create.js
@@ -63,7 +63,7 @@ function Admin_create() {
                 <div className="Formulaire">
                     <form className="form" onSubmit={checkAdd}>
                         <table className="Formulaire_de_connexion">
-                            <tr>
+                            <tr className="one-2">
                                 <div className="texte_côté">
                                     <p>Nom :</p>
                                     <p>Prénom :</p>
@@ -74,7 +74,7 @@ function Admin_create() {
                                     <p>Mot de passe :</p>
                                 </div>
                             </tr>
-                            <tr>
+                            <tr className="two-2">
                                 <input id="nom" name='lastname' className="texte_zone" type="text" placeholder="Nom..." required/>
                                 <input id="prenom" name='firstname' className="texte_zone" type="text" placeholder="Prénom..." required/>
                                 <input id="phone" name='phone' className="texte_zone" type="tel" 
